de41057925dc443248adb3d0c34af9f89cfa59e7
samantrs <https://samantrs@git.rabobank.nl>
2022-10-26 19:44:15 +0200

    movie services added

---
+++ movie-aggregator-service/src/main/java/com/reactive/microservices/movieaggregatorservice/MovieAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.microservices.movieaggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAggregatorServiceApplication.class, args);
+	}
+
+}
---
+++ movie-aggregator-service/src/test/java/com/reactive/microservices/movieaggregatorservice/MovieAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.microservices.movieaggregatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
