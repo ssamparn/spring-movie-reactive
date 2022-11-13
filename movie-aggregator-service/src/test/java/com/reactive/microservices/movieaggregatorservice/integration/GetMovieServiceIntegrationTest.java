e7e242eac8e41afd36100da5957920cc6c47878b
samantrs <https://samantrs@git.rabobank.nl>
2022-11-13 17:14:02 +0100

    movie service integration test for 4xx and 5xx

--- movie-aggregator-service/src/test/java/com/reactive/microservices/movieaggregatorservice/integration/GetMovieServiceIntegrationTest.java
+++ movie-aggregator-service/src/test/java/com/reactive/microservices/movieaggregatorservice/integration/GetMovieServiceIntegrationTest.java
@@ -16,12 +16,35 @@ public class GetMovieServiceIntegrationTest extends AbstractIntegrationTest {
     }
 
     @Test
-    void movie_not_found_ById() {
+    void movie_info_not_found_ById() {
         webTestClient.get()
-                .uri("/api/v1/movies/{movieId}", "movieIdDoesNotExist")
+                .uri("/api/v1/movies/{movieId}", "movieIdDoesNotExist_InInfo")
                 .exchange()
                 .expectStatus()
-                .isNotFound()
-                .expectBody(String.class);
+                .is4xxClientError()
+                .expectBody(String.class)
+                .isEqualTo("MovieInfo not available for MovieId: movieIdDoesNotExist_InInfo");
+    }
+
+    @Test
+    void movie_reviews_not_found_ById() {
+        webTestClient.get()
+                .uri("/api/v1/movies/{movieId}", "movieIdDoesNotExist_InReview")
+                .exchange()
+                .expectStatus()
+                .is4xxClientError()
+                .expectBody(String.class)
+                .isEqualTo("MovieInfo not available for MovieId: movieIdDoesNotExist_InReview");
+    }
+
+    @Test
+    void movie_info_service_not_available() {
+        webTestClient.get()
+                .uri("/api/v1/movies/{movieId}", "movieInfo_Service_Unavailable")
+                .exchange()
+                .expectStatus()
+                .is5xxServerError()
+                .expectBody(String.class)
+                .isEqualTo("Server exception in MoviesInfo serviceMovieInfo Service Unavailable");
     }
 }
